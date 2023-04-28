d04848ec4be774cb12f346cfaa4e9fcb2d02e16e
Conrad Rolf <williamconradrolf@gmail.com>
2023-04-28 17:40:17 -0500

    Created and tested the 'addToCollection' function, successfully adds the object 'album' into the array 'collection'.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,26 @@
 console.log('***** Music Collection *****')
+
+let collection = []
+
+function addToCollection(title, artist, release) {
+    let album = {
+        title, artist, release
+    }
+    collection.push(album);
+    return album;
+}
+addToCollection('American Idiot (Deluxe)', 'Green Day', '2004')
+console.log(collection[0])
+addToCollection('Sleeptalk', 'Dayseeker', '2019')
+addToCollection('Dark Sun', 'Dayseeker', '2022')
+addToCollection('When Life Gives You Lemons, You Paint That Shit Gold', 'Atmosphere', '2008')
+addToCollection('Doom OST', 'Mick Gordan', '2016')
+addToCollection('beerbongs & bentleys', 'Post Malone', '2018')
+addToCollection('Stoney (Deluxe)', 'Post Malone', '2016')
+
+function showCollection(){
+    for (i in collection)
+        console.log(album.title, 'by', album.artist, 'published in', album.release[i])
+}
+showCollection();
+console.log(collection);
